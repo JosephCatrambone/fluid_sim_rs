6a9af42eec633b959fe158bf5c8015cf04aa2fa5
Joseph Catrambone <jo.jcat@gmail.com>
2022-05-29 17:47:16 -0700

    Divergence/grad flow consistency is almost working but has a bug.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,17 @@ mod fluid;
 use minifb::{Key, MouseButton, MouseMode, Window, WindowOptions};
 use crate::fluid::FluidVolume;
 
-const WIDTH: usize = 480;
-const HEIGHT: usize = 480;
-const DOWNSCALE: usize = 5;
-const DIFFUSION_RATE: f32 = 0.5f32;
-const SUBSTEPS: usize = 2;
-const SIMULATION_ITERATIONS: usize = 0;
-const INFLOW_RATE: f32 = 5.0f32;
+const WIDTH: usize = 640;
+const HEIGHT: usize = 640;
+const DOWNSCALE: usize = 16;
+const DIFFUSION_RATE: f32 = 1.0f32;
+const SIMULATION_ITERATIONS: usize = 10;
+const INFLOW_RATE: f32 = 10.0f32;
 
 fn main() {
 	let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-	let mut fluid_tick = fluid::FluidVolume::new(WIDTH/DOWNSCALE, HEIGHT/DOWNSCALE);
-	let mut fluid_tock = fluid::FluidVolume::new(WIDTH/DOWNSCALE, HEIGHT/DOWNSCALE);
+	let mut fluid = fluid::FluidVolume::new(WIDTH/DOWNSCALE, HEIGHT/DOWNSCALE);
+	//let mut last_mouse: (f32, f32) = (0.0, 0.0);
 
 	let mut window = Window::new(
 		"Test - ESC to exit",
@@ -32,23 +31,32 @@ fn main() {
 
 	while window.is_open() && !window.is_key_down(Key::Escape) {
 		// Draw:
-		let fluid:&FluidVolume = &fluid_tick;
-
 		for (idx, pixel) in buffer.iter_mut().enumerate() {
 			let x_screen = idx%WIDTH;
 			let y_screen = idx/WIDTH;
-			*pixel = (fluid.get_density(x_screen/DOWNSCALE, y_screen/DOWNSCALE) * (255f32)).min(255f32) as u32;
+			let pixel_blue = ((fluid.get_density(x_screen/DOWNSCALE, y_screen/DOWNSCALE) * (255f32))).min(255f32) as u32;
+			let pixel_red = ((fluid.get_velocity_x(x_screen/DOWNSCALE, y_screen/DOWNSCALE) * (255f32))).abs().min(255f32) as u32;
+			let pixel_green = ((fluid.get_velocity_y(x_screen/DOWNSCALE, y_screen/DOWNSCALE) * (255f32))).abs().min(255f32) as u32;
+			*pixel = ((0x010000 * pixel_red) + (0x000100 * pixel_green) + (0x000001 * pixel_blue));
 		}
 
 		// Update Inputs and Simulate:
 		let (mx, my) = window.get_mouse_pos(MouseMode::Clamp).unwrap();
-		let btn = window.get_mouse_down(MouseButton::Left);
-		if btn { fluid_tick.set_density(mx as usize/DOWNSCALE, my as usize/DOWNSCALE, INFLOW_RATE + fluid_tick.get_density(mx as usize, my as usize)); }
+		let left_btn = window.get_mouse_down(MouseButton::Left);
+		if left_btn { fluid.set_density(mx as usize/DOWNSCALE, my as usize/DOWNSCALE, INFLOW_RATE + fluid.get_density(mx as usize, my as usize)); }
+		let right_btn = window.get_mouse_down(MouseButton::Right);
+		if right_btn {
+			//let mdx = mx - last_mouse.0;
+			//let mdy = my - last_mouse.1;
+			//last_mouse = (mx, my);
+			let mdx = mx - (WIDTH/2) as f32;
+			let mdy = my - (HEIGHT/2) as f32;
 
-		for _ in 0..SUBSTEPS {
-			fluid_tick.step_density_diffusion(DIFFUSION_RATE, SIMULATION_ITERATIONS, &mut fluid_tock);
-			fluid_tock.step_density_with_velocity(1.0f32, &mut fluid_tick);
+			println!("Fluid velocity: {} {}", &mdx, &mdy);
+			fluid.set_velocity((WIDTH/2)/DOWNSCALE, (HEIGHT/2)/DOWNSCALE, (mdx, mdy));
 		}
+
+		fluid.step(DIFFUSION_RATE, 0.1f32, SIMULATION_ITERATIONS);
 
 		// We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
 		window
